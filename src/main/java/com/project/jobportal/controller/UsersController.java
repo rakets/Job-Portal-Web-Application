de4e9e16342e20577a42ecebd92d3b254fc7f356
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-10-12 00:15:22 +0200

    1) Add pom.xml dependencies; 2) Update application.properties; 3) Develop Registration Repositories and Controller, Registration new User - 13

---
+++ src/main/java/com/project/jobportal/controller/UsersController.java
@@ -0,0 +1,42 @@
+package com.project.jobportal.controller;
+
+import com.project.jobportal.entity.Users;
+import com.project.jobportal.entity.UsersType;
+import com.project.jobportal.repository.UsersRepository;
+import com.project.jobportal.services.UsersService;
+import com.project.jobportal.services.UsersTypeService;
+import jdk.dynalink.linker.LinkerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UsersController {
+    private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
+        this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        List<UsersType> usersTypes = usersTypeService.getAll();
+        model.addAttribute("getAllTypes", usersTypes);
+        model.addAttribute("user", new Users());
+        return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(Users users){
+//        System.out.println("User:: " + users);
+        usersService.addNew(users);
+        return "dashboard";
+    }
+}
---
+++ src/main/java/com/project/jobportal/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.project.jobportal.repository;
+
+import com.project.jobportal.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+}
