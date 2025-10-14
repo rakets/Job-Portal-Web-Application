742a0c5c78c051a1a80f4b90a68d6a4d059df553
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-10-14 17:51:38 +0200

    Add Login/Logout Request Mappings to Controller 21

--- src/main/java/com/project/jobportal/controller/UsersController.java
+++ src/main/java/com/project/jobportal/controller/UsersController.java
@@ -5,8 +5,13 @@ import com.project.jobportal.entity.UsersType;
 import com.project.jobportal.repository.UsersRepository;
 import com.project.jobportal.services.UsersService;
 import com.project.jobportal.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,4 +54,20 @@ public class UsersController {
         usersService.addNew(users);
         return "dashboard";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
+    }
+
 }
