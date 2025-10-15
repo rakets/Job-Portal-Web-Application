08c3402d93cd85b2b55f1dd8e95d392961597f52
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-10-16 00:00:58 +0200

    Recruiter Dashboard Display and Retrieve Jobs 33

--- src/main/java/com/project/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/project/jobportal/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.project.jobportal.controller;
 
 import com.project.jobportal.entity.JobPostActivity;
+import com.project.jobportal.entity.RecruiterJobsDto;
+import com.project.jobportal.entity.RecruiterProfile;
 import com.project.jobportal.entity.Users;
 import com.project.jobportal.services.JobPostActivityService;
 import com.project.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -34,6 +38,10 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUserName = authentication.getName();
             model.addAttribute("username", currentUserName);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
         model.addAttribute("user", currentUserProfile);
         return "dashboard";
