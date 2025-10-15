f07694e48f03e0d1f84ce7780e547f8f2d572614
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-10-16 00:37:16 +0200

    Recruiter Dashboard: Edit a job 34

--- src/main/java/com/project/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/project/jobportal/services/JobPostActivityService.java
@@ -31,4 +31,8 @@ public class JobPostActivityService {
         }
         return recruiterJobsDtoList;
     }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepository.findById(id).orElseThrow(() -> new RuntimeException("Job not found"));
+    }
 }
