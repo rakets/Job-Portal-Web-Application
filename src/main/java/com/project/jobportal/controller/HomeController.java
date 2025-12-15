9634c03c6d2a46f59c7e5c6b31de0a47c6a9105a
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-12-15 19:57:19 +0100

    feat: integration of currency rate API 1) dependency of webflux has been added in pom.xml; 2) class NbpExchangeRateDTO has been created in dto; 3) class NbpExchangeRateServic has been created in service; 4) class HomeController has been changed; 5) file index.html has been changed;

--- src/main/java/com/project/jobportal/controller/HomeController.java
+++ src/main/java/com/project/jobportal/controller/HomeController.java
@@ -1,12 +1,50 @@
 package com.project.jobportal.controller;
 
+import com.project.jobportal.services.NbpExchangeRateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
+    private final NbpExchangeRateService nbpExchangeRateService;
+
+    @Autowired
+    public HomeController(NbpExchangeRateService nbpExchangeRateService) {
+        this.nbpExchangeRateService = nbpExchangeRateService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model) {
+        getCurrencyRate(model);
         return "index";
+    }
+
+    //      получение курсов валют и передача в модель
+    private void getCurrencyRate(Model model) {
+        Optional<Double> euroRate = nbpExchangeRateService.getCurrencyExchangeRate("eur");
+        Optional<Double> usdRate = nbpExchangeRateService.getCurrencyExchangeRate("usd");
+        Optional<Double> gbpRate = nbpExchangeRateService.getCurrencyExchangeRate("gbp");
+
+        if (euroRate.isPresent()) {
+            model.addAttribute("euroRateToPln", String.format("%.2f", euroRate.get()));
+            model.addAttribute("usdRateToPln", String.format("%.2f", usdRate.get()));
+            model.addAttribute("gbpRateToPln", String.format("%.2f", gbpRate.get()));
+
+            System.out.println("euroRateToPln: " + euroRate.get());
+            System.out.println("usdRateToPln: " + usdRate.get());
+            System.out.println("gbpRateToPln: " + gbpRate.get());
+        } else {
+            System.out.println("euroRateToPln: " + euroRate.get());
+            System.out.println("usdRateToPln: " + usdRate.get());
+            System.out.println("gbpRateToPln: " + gbpRate.get());
+
+            model.addAttribute("euroRateToPln", "N/A");
+            model.addAttribute("usdRateToPln", "N/A");
+            model.addAttribute("gbpRateToPln", "N/A");
+        }
     }
 }
